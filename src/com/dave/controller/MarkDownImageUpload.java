fcdd2cca48e0c65fdab5db9be2a824f2d9e3c556
Dave <hellopeiwen@163.com>
2019-11-18 10:38:34 +0800

    修改上传图片路径

--- src/com/dave/controller/MarkDownImageUpload.java
+++ src/com/dave/controller/MarkDownImageUpload.java
@@ -1,15 +1,12 @@
 package com.dave.controller;
 
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import org.json.JSONObject;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,24 +22,21 @@ public class MarkDownImageUpload {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");  
         PrintWriter wirte = null; 
-        Map<String, Object> map = new HashMap<String, Object>();
-//        JSONObject json = new JSONObject();
+        JSONObject json = new JSONObject();
          try {  
                 wirte = response.getWriter();
                 //文件存放的路径
                 String path = request.getSession().getServletContext().getRealPath("upload");
-                String url = "http://localhost:8080" 
-                        + request.getContextPath() 
-                        + "//upload//"
+                String url = "http://localhost:8080" + request.getContextPath() + "/upload/"
                         + FileUpload.upload(request, file, path); 
-                map.put("success", 1);
-                map.put("message", "hello");
-                map.put("url", url);
+                json.put("success", 1);
+                json.put("message", "hello");
+                json.put("url", url);
             } catch (Exception e) {  
-            }finally{  
-                wirte.print(map);
-                wirte.flush();  
-                wirte.close();  
+            } finally {
+                wirte.print(json);
+                wirte.flush();
+                wirte.close();
             }
     }
 
